095330f4cd9d7492a87780fc5076e41ac5dd4c50
IamLAM <ingeniero@luismonge.com.mx>
2019-03-31 11:32:00 -0700

    Using length and brackets

--- basic.js
+++ basic.js
@@ -5,9 +5,13 @@ var name = "Pilar";
 var middle = "Schnaider";
 var p = 'look this "rabbit"';
 var p2 = "\\look this \"lion\"";
-p2 += "A \nnew \tanimal";
 
+
+
+p2 += "A \nnew \tanimal";
 a++; //autoincrement
 console.log(a);
 console.log(b);
 console.log(name + middle);
+console.log(p2.length); //letters count
+console.log(p2[0]); //letters count
