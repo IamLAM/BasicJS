527d4ed5879ea2365fe7d2f7c4a8e79e495a463b
IamLAM <ingeniero@luismonge.com.mx>
2019-04-01 22:59:49 -0700

    Using isOwnProperty function

--- basic.js
+++ basic.js
@@ -226,3 +226,22 @@ cat.run = "too fast";
 console.log(cat);
 delete cat.legs;
 console.log(cat);
+
+// hasOwnProperty
+var myObj = {
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+    // Your Code Here
+    if (myObj.hasOwnProperty(checkProp))
+        return myObj[checkProp];
+    else
+        return "Not Found";
+}
+
+// Test your code by modifying these values
+var check = checkObj("gift");
+console.log(check);
