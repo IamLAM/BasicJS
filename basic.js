45f8cd9bb77e3e016eadce5827b326dd3547f467
IamLAM <ingeniero@luismonge.com.mx>
2019-04-02 00:11:07 -0700

    Checking object list and compare some values

--- basic.js
+++ basic.js
@@ -289,6 +289,56 @@ for (i = 1; i < 5; i++)
 
 do {
 
-    console.log(ja);
+    console.log("ja");
     i++;
 } while (i < 5);
+
+
+//Setup
+var contacts = [{
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop) {
+
+    i = 0;
+    while (i < contacts.length) {
+        if (contacts[i].firstName === name) {
+            if (contacts[i].hasOwnProperty(prop))
+                return contacts[i][prop];
+            else {
+                return "No such property";
+
+            }
+        }
+        i++;
+
+    }
+    return "No such contact";
+}
+
+var resultado = lookUpProfile("Akira", "likes");
+console.log(resultado);
